eaa5f74eab167b0f2b1d1d154e92388b4d518eec
Cr4zyFl1x <flo040703.fkv@gmail.com>
2020-06-22 21:02:43 +0200

    Updated: serveral small fixes

--- _include/initialize.php
+++ _include/initialize.php
@@ -4,7 +4,5 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once 'classes/autoload.php';
-require_once 'config/mySQL.php';
+require_once '_lang/' . $app->utils()->getLanguageCode() . '.php';
 require_once 'siteManager.php';
-exit();
