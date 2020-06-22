91b7950ecd0f08514b434103f3f53021319ffdfb
Cr4zyFl1x <flo040703.fkv@gmail.com>
2020-06-22 21:00:22 +0200

    Added: de_DE Language file German

--- _lang/de_DE.php
+++ _lang/de_DE.php
@@ -25,9 +25,29 @@ $lng['dashboard']['byte'] = 'Byte';
  * Login
  */
 
+$lng['login']['title'] = 'Anmeldung';
+$lng['login']['heading'] = 'Bei Klefiu anmelden';
+$lng['login']['welcome'] = 'Hallo, schön Sie zu sehen!';
 $lng['login']['username'] = 'Benutzername';
 $lng['login']['password'] = 'Passwort';
+$lng['login']['rememberme'] = 'Angemeldet bleiben';
+$lng['login']['forgotpass'] = 'Passwort vergessen?';
 $lng['login']['login'] = 'Anmelden';
+
+/**
+ * Password request
+ */
+
+
+/**
+ * 404 | Page not found
+ */
+
+$lng['404']['title'] = "Seite nicht gefunden";
+$lng['404']['heading'] = "Diese Seite konnte nicht gefunden werden.";
+$lng['404']['description'] = "Die Seite wurde möglicherweise verschoben oder entfernt. Falls du denkst das dies ein Fehler ist, kontaktiere bitte einen Administrator.";
+$lng['404']['back'] = "Zurück";
+
 
 /**
  * Dashboard
