ff671a40883a089208603965ae55f4e36aeb5896
Cr4zyFl1x <flo040703.fkv@gmail.com>
2020-06-25 00:37:56 +0200

    Updated: de_DE.php language file

--- _lang/de_DE.php
+++ _lang/de_DE.php
@@ -15,11 +15,45 @@ $lng = [];
  * Global
  */
 
-$lng['dashboard']['terrabyte'] = 'Terrabyte';
-$lng['dashboard']['gigabyte'] = 'Gigabyte';
-$lng['dashboard']['megabyte'] = 'Megabyte';
-$lng['dashboard']['kilobyte'] = 'Kilobyte';
-$lng['dashboard']['byte'] = 'Byte';
+$lng['panel']['terrabyte'] = 'Terrabyte';
+$lng['panel']['gigabyte'] = 'Gigabyte';
+$lng['panel']['megabyte'] = 'Megabyte';
+$lng['panel']['kilobyte'] = 'Kilobyte';
+$lng['panel']['byte'] = 'Byte';
+$lng['panel']['yes'] = 'Ja';
+$lng['panel']['no'] = 'Nein';
+$lng['panel']['send'] = 'Senden';
+$lng['panel']['back'] = 'Zurück';
+$lng['panel']['confirm'] = 'Bestätigen';
+
+/**
+ * Error
+ */
+
+$lng['error']['login'] = "Der Benutzername oder das Passwort war ungültig.";
+$lng['error']['login_needed'] = "Sie müssen angemeldet sein, um diese Seite aufzurufen!";
+$lng['error']['sessionexpired'] = "Ihre Sitzung ist abgelaufen. Bitte melden Sie sich erneut an.";
+$lng['error']['forgetpassword_delay'] = "Bitte warten Sie <b>{MINUTES}</b> Minuten, bevor Sie einen neuen Link anfordern!";
+$lng['error']['usernotexist'] = "Der Benutzer existiert nicht.";
+$lng['error']['internalerror'] = "Es ist ein Fehler augetreten. Bitte versuchen Sie es später erneut.";
+$lng['error']['invalidtoken'] = "Der Schlüssel ist ungültig oder abgelaufen.";
+$lng['error']['emptystring'] = "Bitte füllen Sie alle Felder aus.";
+$lng['error']['passwordmatch'] = "Die eingegebenen Passwörter stimmen nicht überein!";
+$lng['error']['nopasswordchange'] = "Die eingegebenen Passwörter stimmen mit Ihrem derzeitigen Passwort überein!";
+
+/**
+ * Success
+ */
+
+$lng['success']['linksent'] = 'Der Link wurde an Ihre E-Mail Adresse versandt.';
+$lng['success']['logout'] = 'Sie wurden erfolgreich abgemeldet.';
+$lng['success']['passwordreset'] = 'Ihr Passwort wurde erfolgreich zurückgesetzt.';
+
+/**
+ * Email
+ */
+
+$lng['email']['passwordupdate'] = 'Passwort aktualisierung';
 
 /**
  * Login
@@ -35,9 +69,47 @@ $lng['login']['forgotpass'] = 'Passwort vergessen?';
 $lng['login']['login'] = 'Anmelden';
 
 /**
- * Password request
+ * Forgot password
  */
 
+$lng['forgot_password']['title'] = "Passwort vergessen";
+$lng['forgot_password']['welcome'] = "Fordern Sie einen Link zum zurücksetzten Ihres Passworts an.";
+$lng['forgot_password']['email'] = "E-Mail";
+$lng['forgot_password']['backtologin'] = "Zurück zur Anmeldung";
+
+/**
+ * Reset password
+ */
+
+$lng['reset_password']['title'] = 'Passwort zurücksetzten';
+$lng['reset_password']['welcome'] = 'Setzen Sie nun ein neues Passwort für Ihr Konto.';
+$lng['reset_password']['newpassword'] = 'Neues Passwort';
+$lng['reset_password']['repeatnewpassword'] = 'Neues Passwort wiederholen';
+
+/**
+ * Dashboard
+ */
+
+$lng['dashboard']['downloads'] = 'Downloads';
+$lng['dashboard']['diskusage'] = 'Speicherplatznutzung';
+
+/**
+ * Profile
+ */
+
+$lng['profile']['title'] = 'Profil';
+$lng['profile']['description'] = 'Ihr aktuelles Benutzerprofil können Sie hier verwalten';
+$lng['profile']['uploadedfiles'] = 'Hochgeladene Dateien';
+$lng['profile']['totaldownloads'] = 'Gesamte Downloads';
+$lng['profile']['emailaddress'] = 'E-Mail Adresse';
+$lng['profile']['joindate'] = 'Beitrittsdatum';
+$lng['profile']['address'] = 'Adresse';
+$lng['profile']['overview'] = 'Übersicht';
+$lng['profile']['update'] = 'Aktualisieren';
+$lng['profile']['account'] = 'Konto';
+$lng['profile']['totaldiskspace'] = 'Gesamter Speicherplatz';
+$lng['profile']['useddiskspace'] = 'Genutzer Speicherplatz';
+$lng['profile']['usergroup'] = 'Benutzergruppe';
 
 /**
  * 404 | Page not found
@@ -48,13 +120,6 @@ $lng['404']['heading'] = "Diese Seite konnte nicht gefunden werden.";
 $lng['404']['description'] = "Die Seite wurde möglicherweise verschoben oder entfernt. Falls du denkst das dies ein Fehler ist, kontaktiere bitte einen Administrator.";
 $lng['404']['back'] = "Zurück";
 
-
-/**
- * Dashboard
- */
-
-$lng['dashboard']['downloads'] = 'Downloads';
-$lng['dashboard']['uploads'] = 'Uploads';
 
 
 
