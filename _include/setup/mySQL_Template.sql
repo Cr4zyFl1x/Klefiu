0916872e6fa32e7cebd30400e06187d237550047
Cr4zyFl1x <flo040703.fkv@gmail.com>
2020-06-29 21:06:37 +0200

    Updated: SQL Template

--- _include/setup/mySQL_Template.sql
+++ _include/setup/mySQL_Template.sql
@@ -1,38 +1,44 @@
 CREATE TABLE IF NOT EXISTS `klefiu_users` (
 
-    `ID`            	INT UNSIGNED NOT NULL auto_increment,
-    `email`         	VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `username`      	VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `password`      	VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `ID`            	        INT UNSIGNED NOT NULL auto_increment,
+    `email`         	        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `username`      	        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `password`      	        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
 
-	`salutation`        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `prename`           VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `lastname`          VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `street`            VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `zipCode`           VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `houseNumber`       VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `city`              VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `country`           VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+	`salutation`                VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `prename`                   VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `lastname`                  VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `street`                    VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `zipCode`                   VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `houseNumber`               VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `city`                      VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `country`                   VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
 
-	`profilePicPath`    VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-    `permGroup`         INT NULL DEFAULT NULL,
+	`profilePic`                VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `permGroup`                 INT NULL DEFAULT NULL,
 
-    `diskSpace`         INT(20) NULL DEFAULT '0',
-    `totalDownloads`    INT(20) NULL DEFAULT '0',
+    `totalDiskSpace`            INT(20) NULL DEFAULT '0',
+    `usedDiskSpace`             INT(20) NULL DEFAULT '0',
+    `totalDownloads`            INT(20) NULL DEFAULT '0',
+    `uploadedFiles`             INT(20) NULL DEFAULT '0',
 
-	`pwCode`    		VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-	`pwCode_time`   	TIMESTAMP NULL DEFAULT NULL,
+	`langCode`    		        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+	`sidebarStyle`              VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
 
-    `lastLoginAt`		TIMESTAMP NULL DEFAULT NULL,
-    `createdAt`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updatedAt`     timestamp NULL DEFAULT NULL,
+	`pwCode`    		        VARCHAR(255) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+	`pwCode_time`   	        TIMESTAMP NULL DEFAULT NULL,
 
-    PRIMARY KEY (`id`),
+    `lastLoginAt`		        TIMESTAMP NULL DEFAULT NULL,
+    `createdAt`                 timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`                 timestamp NULL DEFAULT NULL,
+
+    PRIMARY KEY (`ID`),
     UNIQUE (`email`),
     UNIQUE (`username`),
     UNIQUE (`pwCode`)
 
 ) engine=innodb DEFAULT charset=utf8 COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS `klefiu_settings` (
     `ID`                        INT UNSIGNED NULL DEFAULT NULL auto_increment,
@@ -41,8 +47,9 @@ CREATE TABLE IF NOT EXISTS `klefiu_settings` (
     `settingVal`                TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
     `settingDescription`        TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
     `updatedAt`                 TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`settingID`)
+    PRIMARY KEY (`ID`),
+    UNIQUE (`settingID`),
+    UNIQUE (`settingNr`)
 ) engine = innodb charset=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `klefiu_loginSessions` (
@@ -64,4 +71,32 @@ CREATE TABLE IF NOT EXISTS `klefiu_bannedIPs` (
     `userID`            INT(10) NULL DEFAULT NULL,
     `ipAddress`         VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
     `createdAt`         timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) engine=innodb DEFAULT charset=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `klefiu_emailValidation` (
+    `ID`                INT(10) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+    `userID`            INT(10) NULL DEFAULT NULL,
+    `mode`              VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
+    `emailCode`         VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
+    `emailOld`          VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
+    `emailNew`          VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
+    `createdAt`         timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (`emailCode`),
+    UNIQUE (`emailNew`),
+    UNIQUE (`emailOld`),
+    UNIQUE (`userID`)
+
+) engine=innodb DEFAULT charset=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `klefiu_cronjobs` (
+    `ID`                INT(10) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+    `cronjobID`         INT(10) NULL DEFAULT NULL,
+    `cronjobName`       VARCHAR(255) collate utf8_unicode_ci NULL DEFAULT NULL,
+    `delay`             INT(10) NOT NULL,
+    `lastRun`           TIMESTAMP NULL DEFAULT NULL,
+    `createdAt`         timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (`cronjobID`),
+    UNIQUE (`cronjobName`)
 ) engine=innodb DEFAULT charset=utf8 COLLATE=utf8_unicode_ci;
