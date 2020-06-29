6fdb442ff4df28338f9953d10ac972f931775e52
Cr4zyFl1x <flo040703.fkv@gmail.com>
2020-06-29 21:04:58 +0200

    Added: de_DE translation

--- _lang/de_DE.php
+++ _lang/de_DE.php
@@ -27,6 +27,41 @@ $lng['panel']['back'] = 'Zurück';
 $lng['panel']['confirm'] = 'Bestätigen';
 
 /**
+ * Navigation
+ */
+
+$lng['nav']['dashboard'] = 'Kontrollzentrum';
+$lng['nav']['files'] = 'Dateien';
+$lng['nav']['file_browser'] = 'Dateibrowser';
+$lng['nav']['upload_file'] = 'Datei hochladen';
+$lng['nav']['statistics'] = 'Statistiken';
+$lng['nav']['support'] = 'Kundenservice';
+$lng['nav']['tickets'] = 'Tickets';
+$lng['nav']['create_ticket'] = 'Neues Ticket erstellen';
+$lng['nav']['account'] = 'Konto';
+$lng['nav']['profile'] = 'Profil';
+$lng['nav']['profile_picture'] = 'Profilbild';
+$lng['nav']['password'] = 'Passwort';
+$lng['nav']['preferences'] = 'Einstellungen';
+$lng['nav']['sessions'] = 'Sitzungen';
+$lng['nav']['login_history'] = 'Anmeldeverlauf';
+$lng['nav']['settings'] = 'Einstellungen';
+$lng['nav']['panel_settings'] = 'Paneleinstellungen';
+$lng['nav']['mail_settings'] = 'Emaileinstellungen';
+$lng['nav']['security_settings'] = 'Sicherheitseinstellungen';
+$lng['nav']['license_settings'] = 'Lizenzeinstellungen';
+$lng['nav']['other_settings'] = 'Sonstige Einstellungen';
+$lng['nav']['accounts'] = 'Konten';
+$lng['nav']['userlist'] = 'Benutzerliste';
+$lng['nav']['create_user'] = 'Benutzer erstellen';
+$lng['nav']['usergroups'] = 'Benutzergruppen';
+$lng['nav']['tools'] = 'Tools';
+$lng['nav']['send_mail'] = 'E-Mail versenden';
+$lng['nav']['file_templates'] = 'Dateivorlagen';
+$lng['nav']['logout'] = 'Abmelden';
+
+
+/**
  * Error
  */
 
@@ -40,6 +75,12 @@ $lng['error']['invalidtoken'] = "Der Schlüssel ist ungültig oder abgelaufen.";
 $lng['error']['emptystring'] = "Bitte füllen Sie alle Felder aus.";
 $lng['error']['passwordmatch'] = "Die eingegebenen Passwörter stimmen nicht überein!";
 $lng['error']['nopasswordchange'] = "Die eingegebenen Passwörter stimmen mit Ihrem derzeitigen Passwort überein!";
+$lng['error']['emailuse'] = "Die eingegebene E-Mail Adresse wird bereits von einem anderen konto genutzt!";
+$lng['error']['emptyfield'] = "Bitte füllen Sie alle mit einem Stern gekennzeichneten Felder aus.";
+$lng['error']['invalidextension'] = "Die hochgeladene Datei erfüllt die Anforderungen an die Dateiendung nicht!";
+$lng['error']['filesize'] = "Die Dateigröße der hochgeladenen Datei ist zu groß!";
+$lng['error']['nonimage'] = "Die hochgeladene Datei ist kein Bild!";
+$lng['error']['imagedimensions'] = "Das hochgeladene Bild ist nicht quadratisch!";
 
 /**
  * Success
@@ -47,13 +88,16 @@ $lng['error']['nopasswordchange'] = "Die eingegebenen Passwörter stimmen mit Ih
 
 $lng['success']['linksent'] = 'Der Link wurde an Ihre E-Mail Adresse versandt.';
 $lng['success']['logout'] = 'Sie wurden erfolgreich abgemeldet.';
+$lng['success']['profileupdate'] = 'Ihre Kontodetails wurden erfolgreich aktualisiert!';
 $lng['success']['passwordreset'] = 'Ihr Passwort wurde erfolgreich zurückgesetzt.';
+$lng['success']['emailvalidationsent'] = 'Eine E-Mail zur Überprüfung Ihrer Identität wurde an Ihre E-Mail Adresse gesendet. (Ihre E-Mail Adresse wird nach Bestätigung aktualisiert.)';
 
 /**
  * Email
  */
 
 $lng['email']['passwordupdate'] = 'Passwort aktualisierung';
+$lng['email']['emailupdate'] = 'E-Mail Adressen aktualisierung';
 
 /**
  * Login
@@ -102,14 +146,40 @@ $lng['profile']['description'] = 'Ihr aktuelles Benutzerprofil können Sie hier 
 $lng['profile']['uploadedfiles'] = 'Hochgeladene Dateien';
 $lng['profile']['totaldownloads'] = 'Gesamte Downloads';
 $lng['profile']['emailaddress'] = 'E-Mail Adresse';
+$lng['profile']['salutation'] = 'Anrede';
+$lng['profile']['prename'] = 'Vorname';
+$lng['profile']['lastname'] = 'Nachname';
+$lng['profile']['username'] = 'Benutzername';
+$lng['profile']['street'] = 'Straße';
+$lng['profile']['housenumber'] = 'Hausnummer';
+$lng['profile']['zipcode'] = 'Postleitzahl';
+$lng['profile']['country'] = 'Land';
+$lng['profile']['city'] = 'Stadt';
+$lng['profile']['mrs'] = 'Frau';
+$lng['profile']['mr'] = 'Herr';
+$lng['profile']['diversity'] = 'Sonstige';
 $lng['profile']['joindate'] = 'Beitrittsdatum';
 $lng['profile']['address'] = 'Adresse';
 $lng['profile']['overview'] = 'Übersicht';
-$lng['profile']['update'] = 'Aktualisieren';
+$lng['profile']['details'] = 'Kontodetails';
+$lng['profile']['profile_picture'] = 'Profilbild';
+$lng['profile']['password'] = 'Passwort';
+$lng['profile']['newpassword'] = 'Neues Passwort';
+$lng['profile']['repeatnewpassword'] = 'Neues Passwort wiederholen';
+$lng['profile']['preferences'] = 'Einstellungen';
+$lng['profile']['sessions'] = 'Sitzungen';
+$lng['profile']['login_history'] = 'Anmeldeverlauf';
 $lng['profile']['account'] = 'Konto';
 $lng['profile']['totaldiskspace'] = 'Gesamter Speicherplatz';
 $lng['profile']['useddiskspace'] = 'Genutzer Speicherplatz';
 $lng['profile']['usergroup'] = 'Benutzergruppe';
+$lng['profile']['updateprofilepic'] = 'Profilbild aktualisieren';
+$lng['profile']['deleteprofilepic'] = 'Profilbild entfernen';
+$lng['profile']['uploadimage'] = 'Bild hochladen';
+$lng['profile']['nofilepicked'] = 'Kein Bild gewählt';
+$lng['profile']['usernamechange'] = 'Änderung des Benutzernamens';
+$lng['profile']['usernamechangeinfo'] = 'Um eine Änderung des Benutzernamens anzufordern, wenden Sie sich bitte an den Support.';
+$lng['profile']['maximumfilesize'] = 'Maximale Dateigröße 8 MB';
 
 /**
  * 404 | Page not found
